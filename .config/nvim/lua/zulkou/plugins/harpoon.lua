029f15788f54c055b539f2ce7cffc5678a59f738
zulkou <73563319+zulkou@users.noreply.github.com>
2025-01-30 15:59:17 +0700

    chore: change some keymap

--- .config/nvim/lua/zulkou/plugins/harpoon.lua
+++ .config/nvim/lua/zulkou/plugins/harpoon.lua
@@ -10,18 +10,18 @@ return {
 	    -- REQUIRED
 	    harpoon:setup()
 	    -- REQUIRED
-		
+
 	    vim.keymap.set("n", "<leader>A", function() harpoon:list():prepend() end)
 	    vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 	    vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-	    vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-	    vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-	    vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-	    vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
-	    vim.keymap.set("n", "<leader><C-h>", function() harpoon:list():replace_at(1) end)
-	    vim.keymap.set("n", "<leader><C-t>", function() harpoon:list():replace_at(2) end)
-	    vim.keymap.set("n", "<leader><C-n>", function() harpoon:list():replace_at(3) end)
-	    vim.keymap.set("n", "<leader><C-s>", function() harpoon:list():replace_at(4) end)
+	    vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
+	    vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
+	    vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
+	    vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
+	    vim.keymap.set("n", "<leader><C-1>", function() harpoon:list():replace_at(1) end)
+	    vim.keymap.set("n", "<leader><C-2>", function() harpoon:list():replace_at(2) end)
+	    vim.keymap.set("n", "<leader><C-3>", function() harpoon:list():replace_at(3) end)
+	    vim.keymap.set("n", "<leader><C-4>", function() harpoon:list():replace_at(4) end)
     end
 }

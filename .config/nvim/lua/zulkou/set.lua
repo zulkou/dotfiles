3451c6b573c33c39794c7d5823cbf99c355b4330
zulkou <73563319+zulkou@users.noreply.github.com>
2025-01-30 15:56:01 +0700

    fix: enable global clipboard for wsl2

--- .config/nvim/lua/zulkou/set.lua
+++ .config/nvim/lua/zulkou/set.lua
@@ -10,6 +10,8 @@ vim.opt.expandtab = true
 
 vim.opt.smartindent = true
 
+vim.opt.clipboard="unnamedplus"
+
 vim.opt.wrap = false
 
 vim.opt.swapfile = false
@@ -27,3 +29,18 @@ vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
+
+if vim.fn.has('wsl') == 1 then
+    vim.g.clipboard = {
+        name = 'WslClipboard',
+        copy = {
+            ['+'] = 'clip.exe',
+            ['*'] = 'clip.exe',
+        },
+        paste = {
+            ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+            ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        },
+        cache_enabled = 0,
+    }
+end

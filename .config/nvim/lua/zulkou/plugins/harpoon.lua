d5285e437b7360640f11e1cd6903dfa9ea6d9262
zulkou <73563319+zulkou@users.noreply.github.com>
2025-01-31 21:06:40 +0700

    chore: update keymap for harpoon

--- .config/nvim/lua/zulkou/plugins/harpoon.lua
+++ .config/nvim/lua/zulkou/plugins/harpoon.lua
@@ -15,13 +15,13 @@ return {
 	    vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 	    vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-	    vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
-	    vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
-	    vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
-	    vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
-	    vim.keymap.set("n", "<leader><C-1>", function() harpoon:list():replace_at(1) end)
-	    vim.keymap.set("n", "<leader><C-2>", function() harpoon:list():replace_at(2) end)
-	    vim.keymap.set("n", "<leader><C-3>", function() harpoon:list():replace_at(3) end)
-	    vim.keymap.set("n", "<leader><C-4>", function() harpoon:list():replace_at(4) end)
+	    vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+	    vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
+	    vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
+	    vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
+	    vim.keymap.set("n", "<leader><C-h>", function() harpoon:list():replace_at(1) end)
+	    vim.keymap.set("n", "<leader><C-j>", function() harpoon:list():replace_at(2) end)
+	    vim.keymap.set("n", "<leader><C-k>", function() harpoon:list():replace_at(3) end)
+	    vim.keymap.set("n", "<leader><C-l>", function() harpoon:list():replace_at(4) end)
     end
 }
--- .config/nvim/lua/zulkou/remap.lua
+++ .config/nvim/lua/zulkou/remap.lua
@@ -10,10 +10,10 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
+-- vim.keymap.set("n", "<C-h>", "<C-w>h")
+-- vim.keymap.set("n", "<C-l>", "<C-w>l")
+-- vim.keymap.set("n", "<C-j>", "<C-w>j")
+-- vim.keymap.set("n", "<C-k>", "<C-w>k")
 
 vim.keymap.set("x", "<leader>p", [["_dP]])
 

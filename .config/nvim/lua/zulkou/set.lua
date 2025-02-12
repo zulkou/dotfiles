2ce77a7986c1a88da92a7872a8b5e0c21f8f7ec7
zulkou <73563319+zulkou@users.noreply.github.com>
2025-02-12 21:44:41 +0700

    perf: change clipboard from clip.exe to wl-clipoard

--- .config/nvim/lua/zulkou/set.lua
+++ .config/nvim/lua/zulkou/set.lua
@@ -10,8 +10,6 @@ vim.opt.expandtab = true
 
 vim.opt.smartindent = true
 
--- vim.opt.clipboard="unnamedplus"
-
 vim.opt.wrap = false
 
 vim.opt.swapfile = false
@@ -30,19 +28,27 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
---[[
-if vim.fn.has('wsl') == 1 then
-    vim.g.clipboard = {
-        name = 'WslClipboard',
-        copy = {
-            ['+'] = 'clip.exe',
-            ['*'] = 'clip.exe',
-        },
-        paste = {
-            ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-            ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-        },
-        cache_enabled = 0,
-    }
+if vim.fn.has("wsl") == 1 then
+    if vim.fn.executable("wl-copy") == 0 then
+        print("wl-clipboard not found, clipboard integration won't work")
+    else
+        vim.g.clipboard = {
+            name = "wl-clipboard (wsl)",
+            copy = {
+                ["+"] = 'wl-copy --foreground --type text/plain',
+                ["*"] = 'wl-copy --foreground --primary --type text/plain',
+            },
+            paste = {
+                ["+"] = (function()
+                    return vim.fn.systemlist('wl-paste --no-newline|sed -e "s/\r$//"', {''}, 1) -- '1' keeps empty lines
+                end),
+                ["*"] = (function()
+                    return vim.fn.systemlist('wl-paste --primary --no-newline|sed -e "s/\r$//"', {''}, 1)
+                end),
+            },
+            cache_enabled = true
+        }
+    end
 end
-]]--
+
+vim.opt.clipboard="unnamedplus"

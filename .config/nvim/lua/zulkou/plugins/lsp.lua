4cd49123c0cc014f7b95b8042d0170d911d248e4
zulkou <73563319+zulkou@users.noreply.github.com>
2025-11-04 18:08:05 +0700

    fix: fix treesitter's highlight not working

--- .config/nvim/lua/zulkou/plugins/lsp.lua
+++ .config/nvim/lua/zulkou/plugins/lsp.lua
@@ -1,86 +1,47 @@
 return {
     {
         "mason-org/mason-lspconfig.nvim",
-        opts = {
-            ensure_installed = { "lua_ls", "gopls", }
-        },
         dependencies = {
-            { "mason-org/mason.nvim", opts = {} },
+            "mason-org/mason.nvim",
             "neovim/nvim-lspconfig",
         },
-        config = function ()
+        opts = {
+            ensure_installed = { "lua_ls", "gopls" },
+        },
+        config = function(_, opts)
             local mason = require("mason")
             local mason_lspconfig = require("mason-lspconfig")
-            local util = require("lspconfig.util")
+            local lspconfig = require("lspconfig")
+            local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-            mason.setup()
-            mason_lspconfig.setup(opts)
+            mason.setup() -- single, clean setup
+            mason_lspconfig.setup({
+                ensure_installed = opts.ensure_installed,
+            })
 
-              -- Buf Beta LSP (manual, outside Mason)
-            vim.lsp.config["buf_ls"] = {
-                cmd = { "buf", "beta", "lsp", "--timeout=0", "--log-format=text" },
-                filetypes = { "proto" },
-                root_dir = function(fname)
-                    return util.root_pattern("buf.yaml", ".git")(fname)
-                        or vim.fn.getcwd()
+            mason_lspconfig.setup_handlers({
+                function(server_name)
+                    lspconfig[server_name].setup({
+                        capabilities = capabilities,
+                    })
                 end,
-            }
-
-            vim.lsp.enable("buf_ls")
-        end
+            })
+        end,
     },
-    -- Autocompletion w/ blink.cmp
+
     {
-        'saghen/blink.cmp',
-        -- optional: provides snippets for the snippet source
-        dependencies = { 'rafamadriz/friendly-snippets' },
-
-        -- use a release tag to download pre-built binaries
-        version = '1.*',
-        -- and/or build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-        -- build = 'cargo build --release',
-        -- if you use nix, you can build from source using latest nightly rust with:
-        -- build = 'nix run .#build-plugin',
-
-        ---@module 'blink.cmp'
-        ---@type blink.cmp.config
+        "saghen/blink.cmp",
+        dependencies = { "rafamadriz/friendly-snippets" },
+        version = "1.*",
         opts = {
-            -- 'default' (recommended) for mappings similar to built-in completions (c-y to accept)
-            -- 'super-tab' for mappings similar to vscode (tab to accept)
-            -- 'enter' for enter to accept
-            -- 'none' for no mappings
-            --
-            -- all presets have the following mappings:
-            -- c-space: open menu or open docs if already open
-            -- c-n/c-p or up/down: select next/previous item
-            -- c-e: hide menu
-            -- c-k: toggle signature help (if signature.enabled = true)
-            --
-            -- see :h blink-cmp-config-keymap for defining your own keymap
-            keymap = { preset = 'default' },
-
-            appearance = {
-                -- 'mono' (default) for 'nerd font mono' or 'normal' for 'nerd font'
-                -- adjusts spacing to ensure icons are aligned
-                nerd_font_variant = 'mono'
-            },
-
-            -- (default) only show the documentation popup when manually triggered
+            keymap = { preset = "default" },
+            appearance = { nerd_font_variant = "mono" },
             completion = { documentation = { auto_show = false } },
-
-            -- default list of enabled providers defined so that you can extend it
-            -- elsewhere in your config, without redefining it, due to `opts_extend`
             sources = {
-                default = { 'lsp', 'path', 'snippets', 'buffer' },
+                default = { "lsp", "path", "snippets", "buffer" },
             },
-
-            -- (default) rust fuzzy matcher for typo resistance and significantly better performance
-            -- you may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-            -- when the rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-            --
-            -- see the fuzzy documentation for more information
-            fuzzy = { implementation = "prefer_rust_with_warning" }
+            fuzzy = { implementation = "prefer_rust_with_warning" },
         },
-        opts_extend = { "sources.default" }
-    }
+        opts_extend = { "sources.default" },
+    },
 }
--- .config/nvim/lua/zulkou/plugins/treesitter.lua
+++ .config/nvim/lua/zulkou/plugins/treesitter.lua
@@ -3,18 +3,13 @@ return {
     branch = "master",
     lazy = false,
     build = ":TSUpdate",
-    opts = {
-        ensure_installed = { "lua", "python", "go", "c_sharp", "vim", "vimdoc", "query", "markdown", "html", "typescript"},
-        highlight = {
-            enable = true,
-            disable = function(lang, buf)
-                local max_filesize = 100 * 1024
-                local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                if ok and stats and stats.size > max_filesize then
-                    return true
-                end
-            end,
-            additional_vim_regex_highlighting = false,
-        }
-    },
+    config = function ()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = { "lua", "python", "go", "c_sharp", "vim", "vimdoc", "query", "markdown", "html", "typescript"},
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            }
+        })
+    end,
 }
